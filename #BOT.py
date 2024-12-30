90d1bd957bafd19bf863798c4da4b89d8ee31ba7
Silmara Lima <silmara.pereiraspl@gmail.com>
2024-12-30 18:00:38 -0300

    first commit

---
+++ #BOT.py
@@ -0,0 +1,16 @@
+#BOT
+name = input("Hello! What is your name?")
+print({name})
+print(f"Nice to meet you, {name}!")
+
+age_input = input("How old are you?")
+age = int(age_input)
+print("How old are you?")
+
+bot_age = 3
+age_difference = age - bot_age
+print(f"You are {age_difference} years older than me. I'm only {bot_age} years old!")
+
+color_input = input("What's your favorite color? ")
+color = color_input
+print(f"Oh, {color} is a beautiful color!")
